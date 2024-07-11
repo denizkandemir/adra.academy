c4999155c4ccfc4290d7cf6ec145a7f5ee4e79f1
Deniz Kandemir <denizkandemir137@gmail.com>
2024-07-11 15:22:19 +0300

    Adra Academy Header Created

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,28 @@
 import Navbar from "./navbar"
+import logo from "/logo.jpg" 
+import "./Header.css";
+
 
 function Header() {
 
-
-    return(
-      <>
-        <Navbar/>
-      </>
-    )
+  return (
+    <>
+      <Navbar />
+      <div className="header-content-container">
+        <div className="header-p-button-container">
+          <h3 className="header-title"> Lorem ipsum dolor sit, 
+            <br/> amet consectetur adipisicing elit. Placeat eius maxime ,
+            <br/> tempore iste repudiandae in itaque possimus non harum dolor
+             <br/>molestias cumque temporibus.
+          </h3>
+          <button className="header-button"> Kurslarımız </button>
+        </div>
+        <div className="header-logo-container">
+          <img className="header-logo" src={logo} alt="" />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Header;
