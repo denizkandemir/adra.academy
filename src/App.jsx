1950c16a4d243d2338b7c02b658e0f2ca2d12f89
Pelin duman <pelindmn04@gmail.com>
2024-07-14 20:23:38 +0300

    "Biz Kimiz" section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/header'
 
+import Slide from './components/header/slide'
+
+
+
 function App() {
   
 
   return (
    <>
     <Header/>
+    <Slide/>
+   
+
    </>
   )
 }
