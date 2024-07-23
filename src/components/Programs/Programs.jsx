51094fc372eee4e82a7d1ef20d9a40557232e04f
Deniz Kandemir <denizkandemir137@gmail.com>
2024-07-23 18:45:48 +0300

    adra academy title styles added programs created

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,14 @@
+import "./Programs.scss";
+
+const Programs = () => {
+
+    return(
+        <div className="programs-container">
+          <p className="programs-title">  Kurslarımız </p>
+          <div className="courses-container">
+
+          </div>
+        </div>
+
+    )
+}
