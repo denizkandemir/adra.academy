e36586df38280527e8dbe08e3f370a137b49185e
Deniz Kandemir <denizkandemir137@gmail.com>
2024-07-31 07:18:59 +0300

    programs completed

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -3,7 +3,7 @@ import img1 from "/atölye-adra.jpeg"
 import img2 from "/destek-adra.jpeg"
 import img3 from "/öğrenci-koçluğu-adra.jpeg"
 import img4 from "/adra-koç.jpeg"
-
+import BooksPeople from "../svgs/BooksPeople";
 
 const Programs = () => {
 
@@ -37,8 +37,10 @@ const Programs = () => {
 
   return (
     <div className="programs-container">
-
-      <p className="programs-main-title">  Programlarımız </p>
+      <div className="programs-main-title-container">
+        {/* <BooksPeople width={60} height={60}/> */}
+        <p className="programs-main-title">  Kurslarımız </p>
+      </div>
       <div className="courses-container">
         {
           courses.map((course) => (
@@ -48,21 +50,36 @@ const Programs = () => {
                 <p className="courses-title">  {course.title} </p>
                 <div className="programs-bottom-content-container">
                   <div className="programs-svg-p-container">
-                    <p className="programs-p"> Yüz Yüze </p>
+                    <p className="programs-p"> Online </p>
+                    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M4.89163 13.2687L9.16582 17.5427L18.7085 8"
+                        stroke="rgb(255,255,255)"
+                        strokeWidth="2.2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
                   </div>
                   <div className="programs-svg-p-container">
-                    <p className="programs-p"> Online </p>
+                    <p className="programs-p"> Yüz Yüze </p>
+                    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M4.89163 13.2687L9.16582 17.5427L18.7085 8"
+                        stroke="#FFFFFF"
+                        strokeWidth="2.2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
                   </div>
-                  <button className="programs-button"> İncele </button>
-
+                  <button className="programs-button">  Keşfet </button>
                 </div>
               </div>
             </div>
           ))
         }
-
       </div>
-
     </div>
   )
 }
