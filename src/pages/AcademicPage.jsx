041d782ac7e8edbd7dcab994778cfe17ba7576c7
Deniz Kandemir <denizkandemir137@gmail.com>
2024-09-02 01:21:57 +0300

    academic page header added

--- src/pages/AcademicPage.jsx
+++ src/pages/AcademicPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import AcademicPageHeader from "../components/AcademicPageHeader/AcademicPageHeader";
+import Programs from "../components/Programs/Programs";
 
 const AcademicPage = () => {
   return (
-   <div>
-    <a href=""> a</a>
-   </div>
+    <>
+      <AcademicPageHeader/>
+      <Programs/>
+    </>
+  
   )
 
 }
