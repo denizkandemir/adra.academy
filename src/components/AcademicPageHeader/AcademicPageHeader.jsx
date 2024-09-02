348b73415f11a4daa40a4c603a19f575f0547b3a
Deniz Kandemir <denizkandemir137@gmail.com>
2024-09-02 03:33:49 +0300

    Variables added - academic page styles updated

--- src/components/AcademicPageHeader/AcademicPageHeader.jsx
+++ src/components/AcademicPageHeader/AcademicPageHeader.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import "./AcademicPageHeader.scss";
+import CapSvg from "../svgs/CapSvg";
 
 const AcademicPageHeader = () => {
   return (
-   <div className="academic-page-header-container">
-     <div className="academic-title-container">
-       <p className="academic-service-title"> Hizmetlerimiz </p>
-       <h1 className="academic-title">
-         Yabancı Dil Ve Akademik Başarı
-       </h1>
-     </div>
-
-   </div>
+    <div className="academic-page-header-container">
+      <div className="academic-page-content-container">
+        <div className="academic-title-container">
+          <div className="svg-title-container">
+            {/* <CapSvg /> */}
+            {/* <div className="square-div"></div> */}
+            <p className="academic-service-title"> Hizmetlerimiz </p>
+          </div>
+          <h1 className="academic-title">
+            Yabancı Dil Ve Akademik Başarı
+          </h1>
+        </div>
+      </div>
+    </div>
   )
 
 }
