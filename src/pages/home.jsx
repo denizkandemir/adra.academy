d09a51fbe699b6f3f534038c766d53143f21bd4c
Deniz Kandemir <denizkandemir137@gmail.com>
2024-11-08 18:49:02 +0300

    header error corrected

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Header from "../components/Header/Headerr";
 import Slide from "../components/Slide/Slide";
 import Programs from "../components/Programs/Programs";
 import MottoSection from "../components/MottoSection/MottoSection";
 import ContactUs from "../components/ContactUs/ContactUs";
 import Services from "../components/Services/Services";
 import { useEffect } from "react";
+import Headerr from "../components/Header/Headerr";
 
 
 const Home = () => {
@@ -16,7 +16,7 @@ const Home = () => {
 
   return (
     <>
-      <Header />
+      <Headerr />
       <Slide />
       <Services/>
       <MottoSection />
